7a0f6b6089e0a1df61e1a120dd765f4ab3de85a4
jhurtadomije <153659627+jhurtadomije@users.noreply.github.com>
2024-12-30 15:15:00 +0100

    Add files via upload

---
+++ Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+define("SERVERNAME", "localhost");
+define("DATABASE", "clinica2");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("CONTROLLER_DEFAULT", "AuthController");
+define("ACTION_DEFAULT", "index");
+define("BASE_URL", "http://localhost/clinica2/");
+define("EMAIL", "jhurtadomije@gmail.com");
+define("PASS", "14635249G");
+
